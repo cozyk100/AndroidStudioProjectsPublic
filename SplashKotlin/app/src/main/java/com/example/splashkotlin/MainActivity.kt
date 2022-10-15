b0ef4353f78bc27dffbd7e6eca7904895929635f
cozyk100 <cozyk100@gmail.com>
2022-10-15 20:40:10 +0900

    Splashのサンプルkotlin

---
+++ SplashKotlin/app/src/main/java/com/example/splashkotlin/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.splashkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * スプラッシュ・スクリーンの例
+ * https://qiita.com/karass/items/bcb50607c0b9fc21f183
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_SplashKotlin)
+        Thread.sleep(1000) // 早すぎるのでsleep
+        setContentView(R.layout.activity_main)
+    }
+}
